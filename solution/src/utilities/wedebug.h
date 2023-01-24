9054419915835b73b5180aaba59127da41336a8d
zanoo <dmitryellison@gmail.com>
2023-01-24 12:28:45 +0300

    БАЗА

--- solution/src/utilities/wedebug.h
+++ solution/src/utilities/wedebug.h
@@ -2,6 +2,6 @@ void read_status_print(FILE* f, enum read_status rs);
 
 void write_status_print(FILE* f, enum write_status ws);
 
-void static show_header(struct bmp_header const header);
+void show_header(struct bmp_header const header);
 
-void static show_image(FILE* f, struct image const* img);
+void show_image(FILE* f, struct image const* img);
